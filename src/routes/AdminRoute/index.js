4be2844d00163b4baa26be0b3dac730d7eb37df1
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-12 16:54:04 +0700

    protected all admin routes

--- src/routes/AdminRoute/index.js
+++ src/routes/AdminRoute/index.js
@@ -1,7 +1,30 @@
 import { memo } from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { logOutService, getAdminInfoService } from '../../services/authServices'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function AdminRoute() {
-    return <div>Admin</div>
+    const dispatch = useDispatch()
+    const nav = useNavigate()
+    const adminInfo = getAdminInfoService()
+
+    const handleLogOut = () => {
+        logOutService(dispatch)
+        nav('../admin/login')
+    }
+
+    if(adminInfo) {
+        return (
+            <>
+                <button onClick={handleLogOut} >Log out</button>
+                <Outlet />
+            </>
+        )
+    }
+    else{
+        return <Navigate to="../admin/login" />  
+    }
 }
 
 export default memo(AdminRoute)

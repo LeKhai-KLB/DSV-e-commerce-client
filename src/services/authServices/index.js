e1e731e836dd7415e0e5c468be950658ff2abc5a
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-14 18:23:00 +0700

    change code with the changes of auth slice and defined register service

--- src/services/authServices/index.js
+++ src/services/authServices/index.js
@@ -1,24 +1,47 @@
-import { loginStart, loginSuccessful, loginFailure, logOut } from '../../redux/slices/authSlice'
+import { start, successful, logout } from '../../redux/slices/authSlice'
 import axios from 'axios'
 
+// API 
+import { loginAPI, registerAPI } from '../../APIs'
+
 export const loginService = async (dispatch, info) => {
-    dispatch(loginStart())
+    await dispatch(start())
     try{
-        const { data } = await axios.post('/api/user/login', {...info})
+        const { data } = await axios.post(loginAPI, {...info})
         if(data.status === 200) {
-            dispatch(loginSuccessful(data.resultData))
+            dispatch(successful(data.resultData))
+            return {}
         }
         else{
             throw new Error(data.errorMessage)
         }
     }
     catch (err){
-        dispatch(loginFailure(err.message))
+        // console.log(err.message)
+        return {errorMessage: err.message}
     }
 }
 
-export const logOutService = async (dispatch) => {
-    dispatch(logOut())
+export const registerService = async (dispatch, info) => {
+    await dispatch(start())
+    try{
+        const { data } = await axios.post(registerAPI, {...info})
+        if(data.status === 200) {
+            dispatch(successful(data.resultData))
+            return {}
+        }
+        else{
+            throw new Error(data.errorMessage)
+        }
+    }
+    catch (err){
+        // console.log(err.message)
+        return {errorMessage: err.message}
+    }
+}
+
+export const logoutService = async (dispatch) => {
+    dispatch(logout())
 }
 
 export const getUserInfoService = () => {

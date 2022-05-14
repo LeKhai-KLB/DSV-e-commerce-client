3df95f779b099d5817a2feff7bc4d985ba0b4ae6
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-14 18:14:25 +0700

    defined all API path

---
+++ src/APIs/index.js
@@ -0,0 +1,6 @@
+// auth APIs
+export const loginAPI = '/api/user/login'
+export const registerAPI = '/api/user/register'
+
+// categories APIs
+export const getCategoriesByTreeLengthAPI = '/api/general/getCategoriesByTreeLength'

22ff2a23e81cf95b51df943a087837078dbfeb32
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-18 16:25:48 +0700

    add product api

--- src/APIs/index.js
+++ src/APIs/index.js
@@ -2,5 +2,15 @@
 export const loginAPI = '/api/user/login'
 export const registerAPI = '/api/user/register'
 
-// categories APIs
+// general APIs
 export const getCategoriesByTreeLengthAPI = '/api/general/getCategoriesByTreeLength'
+export const getAllBrandsAPI = '/api/general/getAllBrands'
+export const getAllColorsAPI = '/api/general/getAllColors'
+
+// products APIs
+export const getProducts_SortAtoZAPI = '/api/product/admin/getProducts_SortAtoZ'
+export const getProducts_SortZtoAAPI = '/api/product/admin/getProducts_SortZtoA'
+export const getProducts_SortByDateAddedAPI = '/api/product/admin/getProducts_SortByDateAdded'
+export const deleteProductAPI = '/api/product/admin/delete'
+export const addProductAPI = '/api/product/admin/add'
+export const updateProductAPI = '/api/product/admin/update'

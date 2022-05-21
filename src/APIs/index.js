b3bcc230d257ff41aaff40ca3b6e37e415b71bfa
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-21 12:28:44 +0700

    add new API and delete 3 API get products by sort

--- src/APIs/index.js
+++ src/APIs/index.js
@@ -4,13 +4,14 @@ export const registerAPI = '/api/user/register'
 
 // general APIs
 export const getCategoriesByTreeLengthAPI = '/api/general/getCategoriesByTreeLength'
+export const getCategoriesPassByParentAPI = '/api/general/getCategoriesPassByParent'
 export const getAllBrandsAPI = '/api/general/getAllBrands'
+export const getAllBrandPassByCategoryAPI = '/api/general/getAllBrandPassByCategory'
 export const getAllColorsAPI = '/api/general/getAllColors'
 
 // products APIs
-export const getProducts_SortAtoZAPI = '/api/product/admin/getProducts_SortAtoZ'
-export const getProducts_SortZtoAAPI = '/api/product/admin/getProducts_SortZtoA'
-export const getProducts_SortByDateAddedAPI = '/api/product/admin/getProducts_SortByDateAdded'
+export const getProductsByFilterAndSortValue = '/api/product/getProductsByFilterAndSortValue'
+export const getProductByIdAPI = '/api/product/getProductById'
 export const deleteProductAPI = '/api/product/admin/delete'
 export const addProductAPI = '/api/product/admin/add'
 export const updateProductAPI = '/api/product/admin/update'

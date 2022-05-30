62325924e852fa60d884126cd61edcc1ed3ab6e9
LeKhai-KLB <khaileklb1111@gmail.com>
2022-05-30 11:09:36 +0700

    rebuild client pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,86 @@
 import { memo } from "react"
-import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import styles from './home.module.css'
+
+// image assets
+import mainThumb from '../../assets/nonAdmin/main-thumb.jpg'
+import menThumb from '../../assets/nonAdmin/men-thumb.jpg'
+import ladiesThumb from '../../assets/nonAdmin/ladies-thumb.jpg'
+import girlsThumb from '../../assets/nonAdmin/girls-thumb.jpg'
+import boysThumb from '../../assets/nonAdmin/boys-thumb.jpg'
 
 const tempNavigate = ['profile', 'cart']
 
 function Home() {
-    console.log('home')
+    const nav = useNavigate()
+
     return (
         <div className={styles.homePageContainer} >
-            <h1>Home</h1>
-            <ul>
-                {
-                    tempNavigate.map((e, i) => {
-                        return (
-                            <li
-                                key = {i}
-                            >
-                                <Link to = {`/${e}`}>{e}</Link>
-                                <br></br>
-                            </li>
-                        )
-                    })
-                }
-                <li>
-                    product list
-                    <ul>
-                        <li>
-                            men
-                            <ul>
-                                <Link to = "/products/men/short/all_short">short</Link>
-                            </ul>
-                        </li>
-                        <li>lady</li>
-                        <li>boy</li>
-                        <li>girl</li>
-                    </ul>
-                </li>
-            </ul>
+            <div className={styles.topContainer}>
+                <img className={styles.mainImage} src={mainThumb} alt="Main thumb" />
+                <span className={styles.largeTitle}>
+                    Outfit of the week
+                </span>
+                <div 
+                    className={styles.button + ' ' + styles.largeButton} 
+                    onClick={() => nav('../products/ladies/jacket_628609157d8979f4ee873ef2')}
+                >
+                    Shop now
+                </div>
+            </div>
+            <div className={styles.bottomContainer} >
+
+                <div className={styles.subCard} >
+                    <img className={styles.subImage} src={menThumb} alt=" Men thumbnail" />
+                    <div className={styles.smallTitle} >
+                        Men
+                    </div>
+                    <div 
+                        className={styles.button + ' ' + styles.smallButton} 
+                        onClick={() => nav('../products/men/t-shirt_628609b77d8979f4ee873ef3')}
+                    >
+                        Shop now
+                    </div>
+                </div>
+
+                <div className={styles.subCard} >
+                    <img className={styles.subImage} src={ladiesThumb} alt="Ladies thumbnail" />
+                    <div className={styles.smallTitle} >
+                        Ladies
+                    </div>
+                    <div 
+                        className={styles.button + ' ' + styles.smallButton} 
+                        onClick={() => nav('../products/ladies/dress_627f834268f077fbd73d233b')}
+                    >
+                        Shop now
+                    </div>
+                </div>
+
+                <div className={styles.subCard} >
+                    <img className={styles.subImage} src={girlsThumb} alt="Girls thumnail" />
+                    <div className={styles.smallTitle} >
+                        Girls
+                    </div>
+                    <div 
+                        className={styles.button + ' ' + styles.smallButton} 
+                    >
+                        Shop now
+                    </div>
+                </div>
+
+                <div className={styles.subCard} >
+                    <img className={styles.subImage} src={boysThumb} alt="Boys thumbnail" />
+                    <div className={styles.smallTitle} >
+                        Boys
+                    </div>
+                    <div 
+                        className={styles.button + ' ' + styles.smallButton} 
+                    >
+                        Shop now
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,6 +1,6 @@
 import styles from './products.module.css'
-import {Link} from 'react-router-dom'
-import { useParams } from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import { memo, useState, useEffect } from 'react'
 import FilterDropdownBar from '../../components/NonAdminComponent/FilterDropdownBar' 
 import FilterSizeBox from '../../components/NonAdminComponent/FilterBox/FilterSizeBox'
@@ -14,6 +14,7 @@ getAllBrandsAPI,
 getCategoriesPassByParentAPI, 
 getAllBrandPassByCategoryAPI, 
 getProductsByFilterAndSortValue,
+getAllColorsAPI
 } from '../../APIs'
 import axios from 'axios'
 
@@ -22,101 +23,137 @@ import dumpImage  from '../../assets/general/dump/dump1.png'
 
 function Products() {
     const params = useParams()
+    const [queries, setQueries] = useSearchParams()
     const [categoryList, setCategoryList] = useState([])
-    const [currentCategory, setCurrentCategory] = useState(0)
-    const [size, setSize] = useState('s')
-    const [color, setColor] = useState({title: 'black',value: '#000'})
-    const [brand, setBrand] = useState('All')
-    const [available, setAvailable] = useState('All')
-    const [priceRange, setPriceRange] = useState([0, 100])
-    const [sortValue, setSortValue] = useState('Date added')
-    const [page, setPage] = useState(1)
+    const [colorList, setColorList] = useState([])
+    const [brandList, setBrandList] = useState([])
     const [totalLength, setTotalLength] = useState(0)
-    const [productList, setProductList] = useState([
-    ])
+    const [productList, setProductList] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const nav = useNavigate()
 
-    console.log(available)
-
-    const handleClickCategory = (i) => {
-        setCurrentCategory(i)
-    }
-
-    const handleChangePage = (e, p) => {
-        setPage(p)
-    }
-
-    const handleLoadBrandList = async () => {
-        try{
-            console.log(categoryList)
-            const {data} = await axios.get(
-                getAllBrandPassByCategoryAPI + '?category=' + categoryList[currentCategory]?.name.replace(' ', '+')
-            )
-            if(data?.status === 200) {
-                const newBrandList = data.resultData.map(b => b.name)
-                return newBrandList
-            }
-            else {
-                throw new Error(data.errorMessage)
-            }
-        }
-        catch(err) {
-            throw new Error(err.message)
-        }
-    }
-    
     const handleLoadProductList = async () => {
+        setIsLoading(true)
         try {
-            const {data} = await axios.post(getProductsByFilterAndSortValue, 
-            {
-                category: categoryList[currentCategory].name,
-                size: size,
-                color: color.title,
-                brand: brand,
-                available: available,
-                price: `${priceRange[0]}-${priceRange[1]}`,
-                sortValue: sortValue,
-                slice: `${page*20-20}-20`
-            })
+            const queryObject = Object.fromEntries([...queries])
+            let queryString = Object.keys(queryObject).map(key => {
+                return `${key}=${queryObject[key]}`
+            }).join('&')
 
+            if(!queryObject?.category)
+                queryString += '&category=' + params.seRankCategory.split('_')[1]
+            
+            const { data } = await axios.get(getProductsByFilterAndSortValue + '?' + queryString)
             if(data.status === 200) {
                 setProductList(data.resultData.products)
                 setTotalLength(data.resultData.remainingLength)
+                setTimeout(() =>
+                    setIsLoading(false), 300)
             }
             else {
                 throw new Error(data.errorMessage)
             }
         }
-        catch(err) {
+        catch (err) {
             setProductList([])
-            setTotalLength(0)
+            setTimeout(() =>
+                setIsLoading(false), 300)
+            console.log(err.message)
         }
     }
 
-    const handleFirstLoad = async () => {
+    const handleLoadBrandList = async () => {
         try {
-            const { data } = await axios.get(getCategoriesPassByParentAPI + '?parent=' + params.seRankCategory)
-            if(data.status === 200) {
-                setCategoryList([{name: 'All ' + params.seRankCategory}, ...data.resultData])
-                setBrand('All')
+            const categoryId = queries.get('category') ? queries.get('category'):params.seRankCategory.split('_')[1]
+            const brandData = await axios.get(getAllBrandPassByCategoryAPI + '?category=' + categoryId)
+            if(brandData.data.status === 200) {
+                setBrandList(brandData.data.resultData)
             }
         }
+        catch{}
+    }
+    
+    const handleFirstLoad = async () => {
+        try {
+            const parentId = params.seRankCategory.split('_')[1]
+            const categoryData = await axios.get(getCategoriesPassByParentAPI + '?parent=' + parentId)
+            if(categoryData.data.status === 200) {
+                setCategoryList([{name: 'All ' + params.seRankCategory.split('_')[0], _id:parentId}, ...categoryData.data.resultData])
+                
+            }
+            const colorData = await axios.get(getAllColorsAPI)
+            if(colorData.data.status === 200) {
+                setColorList(colorData.data.resultData)
+            }
+            const categoryId = queries.get('category') ? queries.get('category'):params.seRankCategory.split('_')[1]
+            const brandData = await axios.get(getAllBrandPassByCategoryAPI + '?category=' + categoryId)
+            if(brandData.data.status === 200) {
+                setBrandList(brandData.data.resultData)
+            }
+            queries.set('sortValue', '{"title": "Popularity", "key": "sold", "option":"asc"}')
+            queries.set('page', 1)
+            queries.set('limit', 20)
+            setQueries(queries)
+        }
         catch(err) {
-
         }
     }
 
-    useEffect(() => {
-        handleLoadProductList()
-    }, [categoryList, currentCategory, color, brand, priceRange, available, sortValue]);
+    const handleChangeSortValue = (value) => {
+        queries.set('sortValue', value)
+        setQueries(queries)
+    }
+
+    const handleChangeQueryValue = (title ,value) => {
+        if(value === ''){
+            queries.delete(title)
+        }
+        else {
+            queries.set(title, value)
+        }
+        setQueries(queries)
+    }
+
+    const handleChangeCategory = (id) => {
+        {
+            const parentId = params.seRankCategory.split('_')[1]
+            if(id === parentId) {
+                queries.delete('category')
+                setQueries(queries)
+                handleLoadBrandList()
+            }
+            else{
+                queries.set('category', id)
+                setQueries(queries)
+                handleLoadBrandList()
+            }
+        }
+    }
+
+    const handleChangePage = (p) => {
+        queries.set('page', p)
+        setQueries(queries)
+    }
+
+    const handleClickProductCard = (id) => {
+        nav('../product/' + id)
+    }
 
     useEffect(() => {
+        const temp = Object.fromEntries([...queries])
+        if(Object.keys(temp).length !== 0)
+            handleLoadProductList()
+    }, [queries])
+
+    useEffect(() => {
+        console.log(params.seRankCategory)
         handleFirstLoad()
     }, [params.seRankCategory])
     
     return (
-        <div className={styles.productPageContainer}>
+        <div className={styles.productsPageContainer}>
             <div className={styles.pageTitleContainer}>
-                {params.fstRankCategory + ' / ' + params.seRankCategory.replace('_', ' ')}
+                {params.fstRankCategory + ' / ' + params.seRankCategory.split('_')[0].replace('+', ' ')}
             </div>
             <div className={styles.contentContainer}>
 
@@ -127,11 +164,16 @@ function Products() {
                     <div className={styles.categoryBox}>
                         <span className={styles.boxTitle}>Category</span>
                         {
+                            categoryList.length !== 0 &&
                             categoryList.map((e, i) => (
                                 <span 
                                     key={i} 
-                                    className={`${styles.categorySubTitle} ${currentCategory === i ? styles.highlight:''}`}
-                                    onClick={() => handleClickCategory(i)}
+                                    className={`${styles.categorySubTitle} ${
+                                        queries.get('category') ?
+                                        (queries.get('category') === e._id ? styles.highlight:''):
+                                        (params.seRankCategory.split('_')[1] === e._id ? styles.highlight:'')
+                                    }`}
+                                    onClick={() => handleChangeCategory(e._id)}
                                 >
                                     {e.name}
                                 </span>
@@ -147,27 +189,46 @@ function Products() {
 
                         {/* size filter */}
                         <FilterDropdownBar title={'Size'} >
-                            <FilterSizeBox onChange={setSize} value={size}/>   
+                            <FilterSizeBox 
+                                onChange={handleChangeQueryValue} 
+                                value={queries.get('size')}
+                            />   
                         </FilterDropdownBar>
 
                         {/* color filter */}
                         <FilterDropdownBar title={'Color'} >
-                            <FilterColorBox onChange={setColor} value={color} />
+                            <FilterColorBox 
+                                onChange={handleChangeQueryValue} 
+                                value={queries.get('colors') ? queries.get('colors').split('-'):null} 
+                                initData={colorList} 
+                                multiChoice={true}
+                            />
                         </FilterDropdownBar>
 
                         {/* brand filter */}
-                        <FilterDropdownBar title={'Brand'} dependency={[params.seRankCategory, currentCategory]}>
-                            <FilterByWordBox onChange={setBrand} value={brand} handleLoad={handleLoadBrandList} />
+                        <FilterDropdownBar title={'Brand'} >
+                            <FilterByWordBox 
+                                initData={brandList} 
+                                onChange={handleChangeQueryValue} 
+                                title="brands" value={queries.get('brands') ? queries.get('brands').split('-') :null} 
+                            />
                         </FilterDropdownBar>
 
                         {/* price filter */}
                         <FilterDropdownBar title={'Price'} >
-                            <FilterPriceBox value={priceRange} onChange={setPriceRange}/>
+                            <FilterPriceBox 
+                                value={queries.get('price') ? queries.get('price').split('-').map(p => Number(p)):null} 
+                                onChange={handleChangeQueryValue}
+                            />
                         </FilterDropdownBar>
 
                         {/* availabel filter */}
                         <FilterDropdownBar title={'Available'} >
-                            <FilterByWordBox onChange={setAvailable} value={available} initData={['In-store', 'Out of stock']}/>
+                            <FilterByWordBox 
+                            onChange={handleChangeQueryValue} 
+                            title="available" 
+                            value={queries.get('available') ? queries.get('available').split('-'):null} 
+                            initData={['In store', 'Out of stock']}/>
                         </FilterDropdownBar>
 
                     </div>
@@ -179,30 +240,44 @@ function Products() {
 
                     {/* Top products container */}
                     <div className={styles.topProductsContainer}>
-                        <SortBox value={sortValue} onChange={setSortValue} list={['Date added', 'A - Z', 'Z - A']} />
-                        <Pagination count={Math.ceil(totalLength/20)} boundaryCount={1} page={page} onChange={handleChangePage}/>
+                        <SortBox value={queries.get('sortValue')} onChange={handleChangeSortValue} list={[
+                            '{"title": "Popularity", "key": "sold", "option":"asc"}',
+                            '{"title":"Latest", "key": "_id", "option":"desc"}', 
+                            '{"title":"A - Z", "key": "name", "option":"asc"}', 
+                            '{"title":"Z - A", "key": "name", "option":"desc"}',
+                            '{"title":"Price: highest", "key": "price", "option":"desc"}',
+                            '{"title":"Price: lowest", "key": "price", "option":"asc"}'
+                        ]} />
+                        <Pagination count={Math.ceil(Number(totalLength)/20) === 0 ? 1:Math.ceil(Number(totalLength)/20)} boundaryCount={1} page={queries.get('page') ? Number(queries.get('page')):1} onChange={(e, p) => handleChangePage(p)} />
                     </div>
 
                     {/* middle products container */}
                     <div className={styles.middleProductsContainer}>
                         <div className={styles.gridContainer} >
                             {
+                                
                                 productList.length !== 0 ?
                                 productList.map((p, i) => (
-                                    <div key={i} className={styles.productCard} >
+                                    <div key={i} className={styles.productCard} onClick={() => handleClickProductCard(p._id)}>
                                         <img className={styles.cardImage} src={p.images[0]} alt={p.name} />
                                         <div title={p.name} className={styles.cardTitle}>{p.name}</div>
                                         <p className={styles.cardPrice}>${p.price}</p>
                                     </div>
                                 )):
-                                <div className={styles.NofoundMessage}> No result found </div>
+                                <div className={styles.overlayBox}> No result found </div>
                             }
                         </div>
+                        {
+                            isLoading &&
+                            <div className={styles.gridContainer + ' ' + styles.overLayContainer}>
+                                <div className={styles.overlayBox}> Loading... </div>
+                            </div>
+                        }
                     </div>
 
                     {/* bottom products container */}
                     <div className={styles.bottomProductsContainer}>
-                        <Pagination count={Math.ceil(totalLength/20)} boundaryCount={1} page={page} onChange={handleChangePage}/>
+                        <Pagination count={Math.ceil(Number(totalLength)/20) === 0 ? 1:Math.ceil(Number(totalLength)/20)} boundaryCount={1} page={queries.get('page') ? Number(queries.get('page')):1} onChange={(e, p) => handleChangePage(p)} />
                     </div>
                 </div>
             </div>

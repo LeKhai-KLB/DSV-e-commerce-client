e86be97b9e0fd2bc662f10c39c28b5e7ee52bcaa
LeKhai-KLB <khaileklb1111@gmail.com>
2022-06-01 11:06:05 +0700

    delete api define file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <Provider store={store}>
       <PersistGate loading = {null} persistor = {persistor}>
